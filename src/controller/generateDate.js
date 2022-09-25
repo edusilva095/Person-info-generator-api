34f33248bdc9daaecf07d4a3b6f89644e4c0226f
Eduardo Silva <eduardossilva095@gmail.com>
2022-09-25 11:52:02 -0300

    alterando as linhas comentadas

--- src/controller/generateDate.js
+++ src/controller/generateDate.js
@@ -9,7 +9,7 @@ exports.nascimento = () => {
 }
 
 function validateDate(month, year){
-    //verificar qual meses possui 31, 30 e 28 dias
+    //verificar quais meses possui 31, 30 e 28 dias
     if(month == "01" || month == "03" || month == "05" || month == "07" || month == "08" || month == "10" || month == "12"){
         return concantWithZero(Math.floor(Math.random() * 31 + 1));
     } else if(month == "04" || month == "06" || month == "09" || month == "11"){
