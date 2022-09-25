24d5f0617389c9866f768cfe9594b582d499273f
Eduardo Silva <eduardossilva095@gmail.com>
2022-09-25 11:50:20 -0300

    alterando as funções

--- src/controller/generateDate.js
+++ src/controller/generateDate.js
@@ -9,12 +9,13 @@ exports.nascimento = () => {
 }
 
 function validateDate(month, year){
+    //verificar quais meses possui 31, 30 e 28 dias
     if(month == "01" || month == "03" || month == "05" || month == "07" || month == "08" || month == "10" || month == "12"){
         return concantWithZero(Math.floor(Math.random() * 31 + 1));
     } else if(month == "04" || month == "06" || month == "09" || month == "11"){
         return concantWithZero(Math.floor(Math.random() * 30 + 1));
     }else{
-        //verificar se o ano gerado é bissexto
+        //verifica se o ano gerado é bissexto
         if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
             return concantWithZero(Math.floor(Math.random() * 29 + 1));
         }else{
