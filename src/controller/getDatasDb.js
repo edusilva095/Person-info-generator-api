4de588993892393a89268b4986f31eaa35bbdb70
Eduardo Silva <eduardossilva095@gmail.com>
2022-09-25 11:48:16 -0300

    alterando as funções

--- src/controller/getDatasDb.js
+++ src/controller/getDatasDb.js
@@ -2,13 +2,32 @@ const mongoose = require("mongoose");
 const Nome = mongoose.model("nome");
 const Sobrenome = mongoose.model("sobrenome");
 
-exports.getRandomName = async (count) => {
-    let randomNumber = Math.floor(Math.random() * await count);
-    return Nome.findOne().skip(randomNumber);
+exports.getRandomNameOfAnyGender = async (countAllDatas, countMale, countFemale) => {
+    const random = {
+        person: await randomNamesOfAnyGender(countAllDatas),
+        pai: await randomNamesMales(countMale),
+        mae: await randomNamesFemales(countFemale)
+    }
+    return random;
+}
+
+const randomNamesOfAnyGender = async (countAllDatas) => {
+    const names = await Nome.find({});
+    return names[Math.floor(Math.random() * await countAllDatas)]
+}
+
+const randomNamesMales = async (countMale) => {
+    const namesMales = await Nome.find({ "genero": "masculino" });
+    return namesMales[Math.floor(Math.random() * await countMale)]
+}
+
+const randomNamesFemales = async (countFemale) => {
+    const namesFemales = await Nome.find({ "genero": "feminino" });
+    return namesFemales[Math.floor(Math.random() * await countFemale)]
 }
 
 exports.getRandomSobrenome = async (countSobrenome) => {
-    const num = await countSobrenome
+    const num = await countSobrenome;
     var randomNumberOne = Math.floor(Math.random() * num);
     var randomNumberTwo = Math.floor(Math.random() * num);
     let firstSobrenome = await Sobrenome.findOne().skip(randomNumberOne);
