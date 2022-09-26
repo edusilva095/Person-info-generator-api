6345210027c205c9894480bc9e47526b714cae20
Eduardo Silva <eduardossilva095@gmail.com>
2022-09-26 18:15:09 -0300

    alterando funções

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Nome = mongoose.model("nome");
 const Sobrenome = mongoose.model("sobrenome");
-const ListCitys = require("./listCitys");
+const ListCities = require("./listCities");
 const GenerateNames = require("./generateNames");
 const GenerateCpf = require("./generateDocument");
 const GenerateDate = require("./generateDate");
@@ -17,8 +17,7 @@ exports.randomPerson = (req, res) => {
     const date = GenerateDate.nascimento();
     GenerateNames.getRandomNameOfAnyGender(countAllDatas, countMale, countFemale).then((random) => {
         GenerateNames.getRandomSobrenome(countSobrenome).then((sobrenome) => {
-            ListCitys.listCitys().then((city) => {
-                const selectCity = city.data[Math.floor(Math.random() * city.data.length)].microrregiao;
+            ListCities.listCities().then((city) => {
                 const person = {
                     nome: `${random.person.nome} ${sobrenome.lastSobrenomeMother.sobrenome} ${sobrenome.lastSobrenomeFather.sobrenome}`,
                     cpf: GenerateCpf.generateCpf(),
@@ -29,15 +28,16 @@ exports.randomPerson = (req, res) => {
                     altura: (Math.random() * (1.90 - 1.55) + 1.55).toFixed(2), // definindo a altura mínima com 1.55 e altura máxima com 1.90
                     peso: (Math.random() * (90 - 60) + 60).toFixed(2), // definindo o peso mínimo com 60 kg e peso máximo com 90 kg
                     tipo_sanguineo: TypeBlood.TypeBloodArray(),
-                    Estado: selectCity.mesorregiao.UF.nome,
-                    cidade: selectCity.nome,
-                    telefone: GenerateNumberPhone.phone(selectCity),
-                    celular: "",
+                    Estado: city.state,
+                    cidade: city.city,
+                    telefone: GenerateNumberPhone.generateNumberLandline(city.ddd),
+                    celular: GenerateNumberPhone.generateNumberCellPhone(city.ddd),
                     mae: `${random.mae.nome} ${sobrenome.middleSobrenomeMother.sobrenome} ${sobrenome.lastSobrenomeMother.sobrenome}`,
                     pai: `${random.pai.nome} ${sobrenome.middleSobrenomeFather.sobrenome} ${sobrenome.lastSobrenomeFather.sobrenome}`,
                 }
                 res.json(person);
             }).catch((error) => {
+                console.log(error)
                 res.send(error);
             })
         }).catch((error) => {
