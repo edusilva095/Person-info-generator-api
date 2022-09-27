ffddef36e462e4e6893fbc6d00464380c450fef0
Eduardo Silva <eduardossilva095@gmail.com>
2022-09-27 10:41:49 -0300

    nomes de variáveis alteradas

--- src/controller/generateDocument.js
+++ src/controller/generateDocument.js
@@ -1,21 +1,16 @@
-/*
-    esse código não é de minha autoria.
-    esse código foi retirado do https://pt.stackoverflow.com/questions/244457/gerador-de-cpf-em-javascript
-*/
-
-exports.generateCpf = () => {
-    const num1 = aleatorio();
-    const num2 = aleatorio();
-    const num3 = aleatorio();
+exports.generateCpf = (state) => {
+    const num1 = randomThreeNumbers();
+    const num2 = randomThreeNumbers();
+    const num3 = `${randomTwoNumbers()}${taxRegion(state)}`;
     const dig1 = dig(num1, num2, num3);
     const dig2 = dig(num1, num2, num3, dig1);
     return `${num1}.${num2}.${num3}-${dig1}${dig2}`;
 }
 
 exports.generateRg = () => {
-    const num1 = randomFirstPartRg();
-    const num2 = aleatorio();
-    const num3 = aleatorio();
+    const num1 = randomTwoNumbers();
+    const num2 = randomThreeNumbers();
+    const num3 = randomThreeNumbers();
     const num4 = Math.floor(Math.random() * 9);
     return `${num1}.${num2}.${num3}-${num4}`;
 }
@@ -35,12 +30,54 @@ function dig(n1, n2, n3, n4) {
     return y < 2 ? 0 : 11 - y;
 }
 
-function aleatorio() {
-    const aleat = Math.floor(Math.random() * 999);
-    return ("" + aleat).padStart(3, '0');
+const randomThreeNumbers = () => {
+    const randomNumber = Math.floor(Math.random() * 999);
+    return ("" + randomNumber).padStart(3, '0');
 }
 
-function randomFirstPartRg() {
-    const aleat = Math.floor(Math.random() * 99);
-    return ("" + aleat).padStart(2, '0');
+const randomTwoNumbers = () => {
+    const randomNumber = Math.floor(Math.random() * 99);
+    return ("" + randomNumber).padStart(2, '0');
+}
+
+const taxRegion = (state) => {
+    switch (state) {
+        case "Distrito Federal":
+        case "Goiás":
+        case "Mato Grosso do Sul":
+        case "Mato Grosso":
+        case "Tocantis":
+            return 1;
+        case "Acre":
+        case "Amazonas":
+        case "Amapá":
+        case "Pará":
+        case "Rondônia":
+        case "Roraima":
+            return 2;
+        case "Ceará":
+        case "Maranhão":
+        case "Piauí":
+            return 3;
+        case "Alagoas":
+        case "Paraíba":
+        case "Pernambuco":
+        case "Rio Grande do Norte":
+            return 4;
+        case "Bahia":
+        case "Sergipe":
+            return 5;
+        case "Minas Gerais":
+            return 6;
+        case "Espírito Santo":
+        case "Rio de Janeiro":
+            return 7;
+        case "São Paulo":
+            return 8;
+        case "Paraná":
+        case "Santa Catarina":
+            return 9;
+        default:
+            return 0;
+    }
 }
